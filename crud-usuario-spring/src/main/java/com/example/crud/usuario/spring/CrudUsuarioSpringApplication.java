049fe06b4febcd70dd2e6701dd5575701974eea1
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-03-24 17:25:12 -0300

    feat: first commit

---
+++ crud-usuario-spring/src/main/java/com/example/crud/usuario/spring/CrudUsuarioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud.usuario.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsuarioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsuarioSpringApplication.class, args);
+	}
+
+}
---
+++ crud-usuario-spring/src/test/java/com/example/crud/usuario/spring/CrudUsuarioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud.usuario.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsuarioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
